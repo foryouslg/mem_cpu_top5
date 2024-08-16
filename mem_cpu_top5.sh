8b34a7c4e8c1ddc864127d0a9d929dd5d1b4f4a6
oneradish <oneradish@163.com>
2024-08-16 10:30:18 +0800

    mem update mem&cpu

--- mem_cpu_top5.sh
+++ mem_cpu_top5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 输出文件
-output_file="top5_memory_processes.log"
+output_file="top5_mem_CPU_processes.log"
 
 # 创建或清空输出文件
 : > $output_file
@@ -12,16 +12,17 @@ while true; do
 
     # 获取内存占用最高的前5个进程
     top_processes=$(ps aux --sort=-%mem | awk 'NR==1 || NR<=6 {print $0}')
+    #top_processes=$(ps aux --sort=-%cpu | awk 'NR==1 || NR<=6 {print $0}')
 
     # 将结果追加写入输出文件
     {
-        echo "Top 5 memory-consuming processes as of $current_time"
+        echo "Top 5 mem-cpu-consuming processes as of $current_time"
         echo "---------------------------------------------------"
         echo "$top_processes"
         echo
     } >> $output_file
 
-    echo "Recorded top 5 memory-consuming processes at $current_time."
+    echo "Recorded top 5 mem_cpu-consuming processes at $current_time."
 
     # 等待1秒
     sleep 1
